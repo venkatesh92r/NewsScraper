3ccdca1e60b2f52c4b0a2ac5c35798faa85ac41f
VENKATESH R <venkatesh92r@gmail.com>
2018-12-30 09:25:37 +0530

    Refactored application start up class

--- src/main/java/com/github/newsscraper/NewsScraperApplication.java
+++ src/main/java/com/github/newsscraper/NewsScraperApplication.java
@@ -14,28 +14,15 @@ import com.github.newsscraper.entities.NewsItemEntity;
 import com.github.newsscraper.repo.NewsItemRepository;
 import com.github.newsscraper.service.WebScraper;
 
+/**
+*Start-up class of the spring boot application
+*@author venkatesh92r
+*/
 @SpringBootApplication
 public class NewsScraperApplication {
-	
-	
-	
 	public static final String url = "https://www.thehindu.com/archive/web/2010/10/03/";
-
 	public static void main(String[] args) {
-		
-		try {
-			SpringApplication.run(NewsScraperApplication.class, args);
-			
-			
-			
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		//new WebScraper().getPageLinks(url, 0);
-		//SpringApplication.run(NewsScraperApplication.class, args);
+		SpringApplication.run(NewsScraperApplication.class, args);
 	}
-
 }
 
